f44f7eca73859918b73301c260076227d27770c8
bernardoamaral1011 <bernardoamaral1011@gmail.com>
2020-06-14 23:51:46 +0100

    master-corrections

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\ASUS\\Desktop\\Current Projects\\calculaOMeuSalarioMD\\assets\\img\\bg.png":[["bg.35087fa5.png","assets/img/bg.png"],"assets/img/bg.png"],"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
+},{"C:\\Users\\ASUS\\Desktop\\Current Projects\\calculaOMeuSalarioMD\\assets\\img\\bg.jpg":[["bg.c12ef667.jpg","assets/img/bg.jpg"],"assets/img/bg.jpg"],"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
 "use strict";
 
 require("../css/styles.css");
@@ -305,7 +305,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60762" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50181" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
