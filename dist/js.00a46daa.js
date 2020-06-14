c318c9e55475b6f280fb456b7416bd098c9d0828
bernardoamaral1011 <bernardoamaral1011@gmail.com>
2020-06-14 21:19:10 +0100

    master-corrections

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -202,19 +202,21 @@ function calcResult() {
   var cMedioConsulta = document.getElementById("f2").value;
   var nMedioConsultasDia = document.getElementById("f3").value;
   var nMedioConsultasDesmDia = document.getElementById("f4").value;
-  var percentagem = document.getElementById("f5").value / 100; // Verify if any of inputs is nulxl, then output if true
+  var percentagem = document.getElementById("f5").value / 100;
+  var nDiasPSemana = document.getElementById("f6").value; // Verify if any of inputs is nulxl, then output if true
 
   console.log(percentagem);
 
-  if (hSemanais === "" || cMedioConsulta === "" || nMedioConsultasDia === "" || nMedioConsultasDesmDia === "" || percentagem === 0) {
+  if (hSemanais === "" || cMedioConsulta === "" || nMedioConsultasDia === "" || nMedioConsultasDesmDia === "" || percentagem === 0 || nDiasPSemana === "") {
     var warn = document.getElementById("warning");
     warn.innerHTML = "Atenção: Todos os campos devem estar preenchidos!";
   } else {
     var _warn = document.getElementById("warning");
 
     _warn.innerHTML = "";
-    var ordenadoDia = (nMedioConsultasDia - nMedioConsultasDesmDia) * 40 * percentagem;
-    var ordenadoSem = ordenadoDia / 8 * hSemanais;
+    var ordenadoDia = (nMedioConsultasDia - nMedioConsultasDesmDia) * cMedioConsulta * percentagem;
+    var ordenadoSem = ordenadoDia * nDiasPSemana;
+    var valorPHora = ordenadoSem / hSemanais;
     var ordenadoMensalB = ordenadoSem * 4;
     var ordenadoMensalL = 0;
 
@@ -226,15 +228,17 @@ function calcResult() {
 
 
     document.getElementById("o_dia").innerHTML = "Ordenado por dia: ";
-    document.getElementById("o_sem").innerHTML = "Ordenado por semana: ";
+    document.getElementById("o_sem").innerHTML = "Valor por hora: ";
     document.getElementById("o_bru").innerHTML = "Ordenado mensal bruto: ";
     document.getElementById("o_liq").innerHTML = "Ordenado mensal líquido: ";
     document.getElementById("o_dia_v").innerHTML = ordenadoDia.toFixed(0) + "€";
-    document.getElementById("o_sem_v").innerHTML = ordenadoSem.toFixed(0) + "€";
+    document.getElementById("o_sem_v").innerHTML = valorPHora.toFixed(0) + "€";
     document.getElementById("o_bru_v").innerHTML = ordenadoMensalB.toFixed(0) + "€";
     document.getElementById("o_liq_v").innerHTML = ordenadoMensalL.toFixed(0) + "€";
     var comp = ordenadoMensalL - 755.51;
     var aux = "";
+    var aux2 = "";
+    var comp2 = hSemanais * 4 - 40 * 4;
 
     if (comp >= 0) {
       aux = "Ganhas mais " + Math.abs(Math.round((comp + Number.EPSILON) * 100) / 100).toFixed(0) + "€ que o ordenado mínimo!";
@@ -242,7 +246,16 @@ function calcResult() {
       aux = "Ganhas menos " + Math.abs(Math.round((comp + Number.EPSILON) * 100) / 100).toFixed(0) + "€ que o ordenado mínimo!";
     }
 
+    if (comp2 > 0) {
+      aux2 = "Trabalhas mais " + Math.abs(comp2).toFixed(0) + " horas que um trabalhador de ordenado mínimo.";
+    } else if (comp2 < 0) {
+      aux2 = "Trabalhas menos " + Math.abs(comp2).toFixed(0) + " horas que um trabalhador de ordenado mínimo.";
+    } else {
+      aux2 = "Trabalhas o mesmo número de horas que um trabalhador de ordenado mínimo.";
+    }
+
     document.getElementById("comp").innerHTML = aux;
+    document.getElementById("comp2").innerHTML = aux2;
   }
 }
 
@@ -251,6 +264,19 @@ window.isNumberKey = function isNumberKey(evt) {
   if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
   return true;
 };
+/*
+preçoconsulta
+nconsultas
+nº horas por semana
+ndesmarcadas
+nº dias por semana
+percentagem
+
+ordenado por semana -> valor por hora
+
+trabalhas mais X horas que um trabalhador de ordenado mínimo
+
+*/
 },{"../css/styles.css":"css/styles.css"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -279,7 +305,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59572" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60762" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ function calcResult() {
   let nMedioConsultasDia = document.getElementById("f3").value;
   let nMedioConsultasDesmDia = document.getElementById("f4").value;
   let percentagem = document.getElementById("f5").value / 100;
+  let nDiasPSemana = document.getElementById("f6").value;
 
   // Verify if any of inputs is nulxl, then output if true
   console.log(percentagem)
@@ -18,7 +19,8 @@ function calcResult() {
     cMedioConsulta === "" ||
     nMedioConsultasDia === "" ||
     nMedioConsultasDesmDia === "" ||
-    percentagem === 0
+    percentagem === 0 ||
+    nDiasPSemana === ""
   ) {
     let warn = document.getElementById("warning");
     warn.innerHTML = "Atenção: Todos os campos devem estar preenchidos!";
@@ -26,11 +28,12 @@ function calcResult() {
     let warn = document.getElementById("warning");
     warn.innerHTML = "";
 
-    let ordenadoDia =
-      (nMedioConsultasDia - nMedioConsultasDesmDia) * 40 * percentagem;
-    let ordenadoSem = (ordenadoDia / 8) * hSemanais;
+    let ordenadoDia = (nMedioConsultasDia - nMedioConsultasDesmDia) * cMedioConsulta * percentagem;
+    let ordenadoSem = ordenadoDia * nDiasPSemana;
+    let valorPHora = ordenadoSem / hSemanais;
     let ordenadoMensalB = ordenadoSem * 4;
     let ordenadoMensalL = 0;
+
     if (ordenadoMensalB * 12 >= 10000) {
       ordenadoMensalL =
         ordenadoMensalB -
@@ -42,12 +45,12 @@ function calcResult() {
 
     // colocar resultados nos p's
     document.getElementById("o_dia").innerHTML = "Ordenado por dia: ";
-    document.getElementById("o_sem").innerHTML = "Ordenado por semana: ";
+    document.getElementById("o_sem").innerHTML = "Valor por hora: ";
     document.getElementById("o_bru").innerHTML = "Ordenado mensal bruto: ";
     document.getElementById("o_liq").innerHTML = "Ordenado mensal líquido: ";
 
     document.getElementById("o_dia_v").innerHTML = ordenadoDia.toFixed(0) + "€";
-    document.getElementById("o_sem_v").innerHTML = ordenadoSem.toFixed(0) + "€";
+    document.getElementById("o_sem_v").innerHTML = valorPHora.toFixed(0) + "€";
     document.getElementById("o_bru_v").innerHTML =
       ordenadoMensalB.toFixed(0) + "€";
     document.getElementById("o_liq_v").innerHTML =
@@ -55,20 +58,40 @@ function calcResult() {
 
     let comp = ordenadoMensalL - 755.51;
     let aux = "";
+    let aux2 = "";
+    let comp2 = hSemanais*4 - 40*4;  
 
     if (comp >= 0) {
       aux =
         "Ganhas mais " +
         Math.abs(Math.round((comp + Number.EPSILON) * 100) / 100).toFixed(0) +
         "€ que o ordenado mínimo!";
-    } else {
+    } else{
       aux =
         "Ganhas menos " +
         Math.abs(Math.round((comp + Number.EPSILON) * 100) / 100).toFixed(0) +
         "€ que o ordenado mínimo!";
     }
 
+
+    if (comp2 > 0) {
+      aux2 =
+        "Trabalhas mais " +
+        Math.abs(comp2).toFixed(0) +
+        " horas que um trabalhador de ordenado mínimo.";
+    } else if(comp2 < 0){
+      aux2 =
+        "Trabalhas menos " +
+        Math.abs(comp2).toFixed(0) +
+        " horas que um trabalhador de ordenado mínimo.";
+    } else {
+      aux2 =
+        "Trabalhas o mesmo número de horas que um trabalhador de ordenado mínimo.";
+    }
+
     document.getElementById("comp").innerHTML = aux;
+    document.getElementById("comp2").innerHTML = aux2;
+
   }
 }
 
@@ -78,3 +101,17 @@ window.isNumberKey = function isNumberKey(evt) {
 
   return true;
 };
+
+/*
+preçoconsulta
+nconsultas
+nº horas por semana
+ndesmarcadas
+nº dias por semana
+percentagem
+
+ordenado por semana -> valor por hora
+
+trabalhas mais X horas que um trabalhador de ordenado mínimo
+
+*/
