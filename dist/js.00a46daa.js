625c7dd6ab22096615fd191e2bde1f1916331a64
bernardoamaral1011 <bernardoamaral1011@gmail.com>
2020-06-15 18:31:44 +0100

    Page ready with no optimizations

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -305,7 +305,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55615" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49203" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
